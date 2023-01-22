92c441c57e70b0f631144d9f8a39abac08b1fe7d
Akandra-Pawitra <akandrapawitra@gmail.com>
2023-01-22 18:38:20 +0700

    Add scoring logic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react'
-import { Memory } from './components/memory'
+import { React, useState } from 'react'
+import { Memory, Score } from './components/memory'
 
 const id = []
 for (let i = 1; i < 10; i++) id.push('card' + i)
 
 function App () {
+  const [current, setCurrent] = useState(0)
+  const [best, setBest] = useState(0)
+
+  const setScore = () => {
+    setCurrent(current + 1)
+    if (current === best) {
+      setBest(best + 1)
+    }
+  }
+
+  const resetScore = () => {
+    setCurrent(0)
+  }
+
   return (
     <div id='wrapper'>
       <h1 id='title'>MEMORY CARD</h1>
-      <div id='score'>
-        <div id='current'>Score:</div>
-        <div id='highest'>Highest Score:</div>
-      </div>
-      <Memory list={id} />
+      <Score scores={{ current, best }} />
+      <Memory list={id} setScore={setScore} resetScore={resetScore}/>
     </div>
   )
 }
--- src/components/memory.js
+++ src/components/memory.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import { React, useState } from 'react'
 import PropTypes from 'prop-types'
 
 export function Memory (props) {
+  const [clicked, setClicked] = useState([])
+
+  const getScore = (event) => {
+    const card = event.target.id
+    if (!clicked.includes(card)) {
+      props.setScore()
+      const arr = clicked
+      arr.push(card)
+      setClicked(arr)
+    } else {
+      setClicked([])
+      props.resetScore()
+    }
+  }
+
   const cards = props.list.map((card) => {
     return (
-      <span key={card.toString()} id={card}>
+      <span
+        key={card.toString()}
+        id={card}
+        onClick={getScore}
+      >
         { card }
       </span>
     )
@@ -15,5 +34,20 @@ export function Memory (props) {
 }
 
 Memory.propTypes = {
-  list: PropTypes.array
+  list: PropTypes.array,
+  setScore: PropTypes.func,
+  resetScore: PropTypes.func
+}
+
+export function Score (props) {
+  return (
+    <div id='score'>
+        <div id='current'>Score: {props.scores.current}</div>
+        <div id='highest'>Best Score: {props.scores.best}</div>
+      </div>
+  )
+}
+
+Score.propTypes = {
+  scores: PropTypes.object
 }
